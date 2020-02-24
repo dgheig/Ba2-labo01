9ab1c438f9a8beadf6333f5263030c275a788043
Gallay David <david.gallay@heig-vd.ch>
2020-02-24 15:07:53 +0100

    add documentation

---
+++ doc/doxy/html/search/all_3.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['getbase',['getBase',['../classTriangle.html#aae4fecc31aac51c2db24b56e2e061b8c',1,'Triangle']]],
+  ['getcolor',['getColor',['../classCircle.html#a533bcda82eba1757edfc25684e7646f8',1,'Circle::getColor()'],['../classColor.html#a1c76b438e11c54e728b27e3c7a222675',1,'Color::getColor()'],['../classRectangle.html#a5f3f89f9397df4ddcc064ae25e280d5c',1,'Rectangle::getColor()'],['../classSquare.html#ad6707d4fa8ad79ebdc2558602695632b',1,'Square::getColor()'],['../classTriangle.html#a1c63c558a902f71a26bd72680c8db29c',1,'Triangle::getColor()']]],
+  ['getheight',['getHeight',['../classRectangle.html#ae2fb8da7f67a36a80d8f789eec63a074',1,'Rectangle::getHeight()'],['../classTriangle.html#a87849ce466ed942076f1cece11047981',1,'Triangle::getHeight()']]],
+  ['getname',['getName',['../classColor.html#ad12970fef3d1a60a450a9e3cee1057fb',1,'Color']]],
+  ['getradius',['getRadius',['../classCircle.html#a58eb59828b4459ca38932b87c65f03a9',1,'Circle']]],
+  ['getside',['getSide',['../classSquare.html#ab670bd5d9126b09ebd7869670fb8f7c6',1,'Square']]],
+  ['getsurface',['getSurface',['../classCircle.html#a1c83fc4e8df86bc91239462210f3b2d3',1,'Circle::getSurface()'],['../classRectangle.html#addf6cfd9097d92677f6a76f591b88e22',1,'Rectangle::getSurface()'],['../classSquare.html#a5d71637e24b4fb2438fc6281f969837f',1,'Square::getSurface()'],['../classTriangle.html#a43ee8a9653872903a782615d2fc5e977',1,'Triangle::getSurface()']]],
+  ['getwidth',['getWidth',['../classRectangle.html#a0d38078e09cd3151d3009eed3a449660',1,'Rectangle']]],
+  ['green',['GREEN',['../classColor.html#a20a7b04657c1d83fae5d54514d3f1622a9de0e5dd94e861317e74964bed179fa0',1,'Color']]]
+];
